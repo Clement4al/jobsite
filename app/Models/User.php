05300e5377db895d11590508f57e163c089a3ea7
Clement4al <michaelokonclement@gmail.com>
2025-06-05 22:00:34 +0100

    Complete Project

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -33,6 +34,10 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function employer(): HasOne
+    {
+        return $this->hasOne(Employer::class);
+    }
     /**
      * Get the attributes that should be cast.
      *
---
+++ resources/views/components/forms/button.blade.php
@@ -0,0 +1,4 @@
+<button {{ $attributes(['class' => 'bg-blue-800 hover:bg-blue-600 rounded py-2 px-6 font-bold']) }}>{{ $slot }}</button>
+<!-- <button {{$attributes->merge(['class' => 'rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-200', 'type' => 'submit'])}}>
+    {{ $slot }}
+</button> -->
